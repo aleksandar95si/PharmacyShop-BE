0495bfdef86b5e7520125fc0d7c7e33a00013a06
aleksandar95si <a.risticc95@gmail.com>
2021-01-26 10:40:08 +0100

    feat(registry-server): init implementation

---
+++ registry-server/src/test/java/fon/master/nst/registryserver/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.master.nst.registryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
