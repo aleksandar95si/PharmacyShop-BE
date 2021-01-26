d0a86110d20eb397236b69b18ac965c6f4b330c5
aleksandar95si <a.risticc95@gmail.com>
2021-01-26 11:12:03 +0100

    feat(user-service): init implementation

---
+++ user-service/src/test/java/fon/master/nst/apigateway/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.master.nst.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
